47f912c0b84f3dd03d60ea8118a5b31bb6d72421
ManeLippert <manuel-lippert@outlook.de>
2021-09-13 16:27:45 +0200

    Aktualisierung Readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PraktikumsVorlage
-Vorlage für das Physikalische Praktikum an der Universität Bayreuth.
+Latex-Vorlage für das physikalische Praktikum an der Universität Bayreuth zum Zusammenarbeiten über GitHub und gegebenfalls VS-Code.
+
+Gitignore:
+Die .gitignore Datei filtert alle Nebenlatex-Dateien und verhindert ein Commit dieser, da sonst nur Merge-Konflikte entstehen. Bei pdf-Dateien werden nur die Versuch_*.pdf Dateien gefiltert, wenn dies anders gewünscht ist einfach selbst abändern.
+
+Struktur:
+Jedes Kapitel hat einen Input-File. Um Merge-Konflikte zu verhindern wird angeraten für jede Datei eine eigene Tex Datei zu machen und sie im jeweiligen Kapitel zu importieren. Die in Input-Struktur dient zur besseren Übersicht und für mögliche Ordner. Die Zahlen vor den Dateinamen dient zur Ordnung der einzelnen tex-Files nach Kapiteln
+
+Versionen:
+Es gibt eine Version mit Ordnerstruktur und ohne, je nach Präferenz.
---
+++ csv_generator.py
@@ -0,0 +1,49 @@
+Autor: Manuel Lippert
+
+import pandas as pd
+
+print('Hello, this program helps by the creation of csv-files.')
+print('------------SETUP------------')
+
+name = input('Name of File  | ')
+if '.csv' in name:
+    filename = name
+else:
+    filename = name + '.csv'
+
+col_count = int(input('Number of Col | '))
+i = 0 
+col_names = []
+while i < col_count:
+    col_name = input('Name of Col '+str(i+1)+' | ')
+    col_names.append(col_name)
+    i += 1
+
+print('------------DATA-------------')
+
+with open(filename, 'w', newline='') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    filewriter.writerow(col_names) #header
+
+    write_data = True
+    row_count = 0
+
+    while write_data:
+
+        i = 0
+        data = []
+
+        while i < col_count:
+            data_elem = input('Row '+str(row_count)+' ; Col '+ str(i)+' | ')
+            if 'stop' in data_elem:
+                write_data = False
+                data.clear()
+                break
+            else:
+                data.append(data_elem)
+            i += 1
+        
+        filewriter.writerow(data)
+        row_count += 1
+        print('-----------------------------')
